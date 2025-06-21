bacac2bb8becf920528553fee8e9e70e1533e454
kostiantyn-oa <kostiantyn.ryzhykov@oa.edu.ua>
2025-06-21 16:51:13 +0000

    Лабораторна 15. Vue, API

---
+++ app/Http/Controllers/Api/Blog/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\Blog;
+
+use App\Http\Controllers\Controller;
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = BlogPost::with(['user', 'category'])->get();
+
+        return response()->json([
+            'success' => true,
+            'data' => $posts
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Blog\PostController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('blog/posts', [\App\Http\Controllers\Api\Blog\PostController::class, 'index']);
